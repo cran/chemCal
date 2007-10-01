a06c17d2db34e65d526de9cda8d8bb07e311a60b
Johannes Ranke <jranke@uni-bremen.de>
2007-10-01 00:00:00 +0000

    version 0.1-26

--- R/se.predict.nls.R
+++
@@ -1,6 +0,0 @@
-.se.predict.nls <- function(object, newdata)
-{
-  se <- sqrt(apply(attr(predict(object,newdata),"gradient"),1, 
-           function(x) sum(vcov(object)*outer(x,x))))
-  return(se)
-}
